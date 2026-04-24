9da2e349f5a6eff5dbb59b8baec91e6758844f30
Cleber Santz <clebersantz@users.noreply.github.com>
2026-04-23 21:15:07 -0300

    Update main.rs working GUI window
    - Uses winit 0.29 for native Wayland/X11 backend
    - Window created with 1280x720 size
    - Event loop handles close and resize

--- chart_player_rust/src/main.rs
+++ chart_player_rust/src/main.rs
@@ -7,24 +7,31 @@ fn main() {
     
     use winit::event_loop::EventLoop;
     use winit::window::WindowBuilder;
+    use winit::dpi::LogicalSize;
     
     let event_loop = EventLoop::new().expect("Failed to create EventLoop");
     
     let window = WindowBuilder::new()
-        .with_title("ChartPlayer")
-        .with_inner_size(winit::dpi::LogicalSize::new(1280, 720))
+        .with_title("CHARTPLAYER")
+        .with_inner_size(LogicalSize::new(1280.0, 720.0))
+        .with_visible(true)
         .build(&event_loop)
         .expect("Failed to create window");
     
-    println!("Window: {}x{}", window.inner_size().width, window.inner_size().height);
-    println!("Open on desktop - press close to exit\n");
+    println!("Window: {}x{}", 
+        window.inner_size().width, 
+        window.inner_size().height);
+    println!("Running - close window to exit\n");
     
     let _ = event_loop.run(move |event, _target| {
         match event {
             winit::event::Event::WindowEvent { window_id: _, event } => {
                 match event {
                     winit::event::WindowEvent::CloseRequested => {
-                        println!("Exiting...");
+                        println!("Exit");
+                    }
+                    winit::event::WindowEvent::Resized(s) => {
+                        println!("Resize: {}x{}", s.width, s.height);
                     }
                     _ => {}
                 }
